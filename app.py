058124cf729b1a1e6cd88884e5928a15acf176b6
Pau Casacuberta Orta <pauapplecat@me.com>
2017-10-14 23:03:58 +0200

    fora /

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def ocr():
     })
 
 
-@app.route('/transcribe/config/')
+@app.route('/transcribe/config')
 def transcribe_conf():
     """
     URL d'inici = https://twist-transcribe.herokuapp.com/?install_id=30078&user_id=57010&post_data_url=https%3A%2F%2Ftwistapp.com%2Fapi%2Fv2%2Fintegration_incoming%2Fpost_data%3Finstall_id%3D30078%26install_token%3D30078_40c17672a965cca1dae424de0baac187&user_name=Pau+C.
@@ -133,7 +133,7 @@ def transcribe_conf():
     user_name = request.args.get('user_name')
     return "<h1>Welcome to this integration</h1>" + "<p>"+ user_name +"</p>"
 
-@app.route('/ocr/config/')
+@app.route('/ocr/config')
 def ocr_conf():
     """
     URL d'inici = https://twist-transcribe.herokuapp.com/?install_id=30078&user_id=57010&post_data_url=https%3A%2F%2Ftwistapp.com%2Fapi%2Fv2%2Fintegration_incoming%2Fpost_data%3Finstall_id%3D30078%26install_token%3D30078_40c17672a965cca1dae424de0baac187&user_name=Pau+C.
