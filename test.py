03125ec028a326ab00bf1040317dcaaee12dede2
Pau Casacuberta Orta <pauapplecat@me.com>
2017-10-14 15:01:18 +0200

    0

--- test.py
+++ test.py
@@ -59,7 +59,7 @@ def incoming():
         message = "Transcribed text"
         data = r.json()
         print(r.text)
-        image_url = data['comment']['attachments']['image']
+        image_url = data['comment']['attachments'][0]['image']
         print(image_url)
         '''
         # The URL of a JPEG image containing handwritten text.
