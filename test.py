42ab01d929bf2a24f72345fbbb2f29ce461839b2
Pau Casacuberta Orta <pauapplecat@me.com>
2017-10-14 08:59:08 +0200

    Upload app

---
+++ test.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Start video conversations from Twist by just typing `/appear room-name`
+"""
+import os
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route('/applet_appear/incoming', methods=['POST'])
+def incoming():
+    event_type = request.form['event_type']
+
+    if event_type == 'ping':
+        return jsonify({'content': 'pong'})
+    else:
+        content = request.form['content']
+        command = request.form['command']
+        command_argument = request.form['command_argument']
+
+        appear_url =  'https://appear.in/%s' % command_argument
+
+        content = content.replace(u'%s %s' % (command, command_argument),
+                                  u'📹 [%s](%s)' % (command_argument, appear_url))
+
+        return jsonify({
+            'content': content,
+        })
+@app.route('/', methods=['GET'])
+def conf():
+    return "Benvingut a l'integració"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
