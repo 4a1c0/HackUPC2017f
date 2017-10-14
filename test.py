3032563a930b93041d42a19f1d16acb19f81a824
Pau Casacuberta Orta <pauapplecat@me.com>
2017-10-14 14:37:16 +0200

    afegit proves de l'api microsoft

--- test.py
+++ test.py
@@ -9,12 +9,26 @@ from flask import jsonify
 from flask import request
 import requests
 import json
+##import http.client, urllib.request, urllib.parse, urllib.error, base64, requests, time
 
 app = Flask(__name__)
+
+subscription_key = '13hc77781f7e4b19b5fcdd72a8df7156' #Microsoft
+uri_base = 'westcentralus.api.cognitive.microsoft.com'
+requestHeaders = {
+    # Request headers.
+    # Another valid content type is "application/octet-stream".
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
 
 
 @app.route('/transcribe/incoming', methods=['POST'])
 def incoming():
+    # For printed text, set "handwriting" to false.
+    params = {'handwriting' : 'true'}
+
     event_type = request.form['event_type']
 
     if event_type == 'ping':
@@ -40,15 +54,55 @@ def incoming():
         url = 'https://api.twistapp.com/api/v2/comments/getone'
         comment_id = {'id': request.form['comment_id']}
 
-        response = requests.get(url, headers=header, params=comment_id)
+        r = requests.get(url, headers=header, params=comment_id)
 
         message = "Transcribed text"
-        data = load.json(response)
+        data = load.json(r)
         image_url = data['image']
         print(image_url)
+        '''
+        # The URL of a JPEG image containing handwritten text.
+        body = {'url' : image_url}
 
+        try:
+            # This operation requrires two REST API calls. One to submit the image for processing,
+            # the other to retrieve the text found in the image. 
+            #
+            # This executes the first REST API call and gets the response.
+            response = requests.request('POST', uri_base + '/vision/v1.0/RecognizeText', json=body, data=None, headers=requestHeaders, params=params)
+
+            # Success is indicated by a status of 202.
+            if response.status_code != 202:
+                # if the first REST API call was not successful, display JSON data and exit.
+                parsed = json.loads(response.text)
+                print ("Error:")
+                print (json.dumps(parsed, sort_keys=True, indent=2))
+                exit()
+
+            # The 'Operation-Location' in the response contains the URI to retrieve the recognized text.
+            operationLocation = response.headers['Operation-Location']
+
+            # Note: The response may not be immediately available. Handwriting recognition is an
+            # async operation that can take a variable amount of time depending on the length
+            # of the text you want to recognize. You may need to wait or retry this GET operation.
+
+            print('\nHandwritten text submitted. Waiting 10 seconds to retrieve the recognized text.\n')
+            time.sleep(10)
+
+            # Execute the second REST API call and get the response.
+            response = requests.request('GET', operationLocation, json=None, data=None, headers=requestHeaders, params=None)
+
+            # 'data' contains the JSON data. The following formats the JSON data for display.
+            parsed = json.loads(response.text)
+            print ("Response:")
+            print (json.dumps(parsed, sort_keys=True, indent=2))
+
+        except Exception as e:
+            print('Error:')
+            print(e)
+        '''
         content = content.replace(u'%s %s' % (command, command_argument),
-                                  u'%s' % (message))
+                                  u'%s %s' % (message, image_url))
 
         return jsonify({
             'content': content,
